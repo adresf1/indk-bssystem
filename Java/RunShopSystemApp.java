2c87ed9c472dd21c7b31b84b054614e50a8b5f6c
NikkyAS <nikolai64000@gmail.com>
2024-04-17 15:13:05 +0200

    17-04-24. Also created a factory for the client and a way to start the client. The client doesn't immediately just give the exit code 130 something but now gives errors in the SocketClient buyProduct method at line 73. It's a RuntimeException

--- Java/RunShopSystemApp.java
+++ Java/RunShopSystemApp.java
@@ -1,11 +1,14 @@
 import Core.ClientFactory;
 import Network.SocketClient;
+import Shared.TransferObject.Product;
 import javafx.application.Application;
 
 public class RunShopSystemApp {
     public static void main(String[] args)
     {
 
-        ClientFactory cf = new ClientFactory();
+        SocketClient client = new SocketClient();
+        client.buyProduct(new Product("Example Product", 10.0, 1));
+        client.startClient();
     }
 }
