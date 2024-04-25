affd270011476b0bf91d77b3a5c803fb1b3c4d96
Sakaadar <79919443+Sakaadar@users.noreply.github.com>
2024-04-25 16:42:29 +0200

    Test for Organic klassen lavet på relevante metoder

--- Java/Shared/Util/Organic.java
+++ Java/Shared/Util/Organic.java
@@ -1,10 +1,30 @@
 package Shared.Util;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Organic implements Certification{
     public String id, database, description;
-    public MyDate certificationDate, expirationDate;
+    public MyDate certificationDate, expirationDate; //CertificationDate er startdatoen
 
     public String originCountry, orginization;
+
+    public Organic(String id, String database, String description,
+        MyDate certificationDate, MyDate expirationDate, String originCountry,
+        String orginization)
+    {
+        this.id = id;
+        this.database = database;
+        this.description = description;
+        this.certificationDate = certificationDate;
+        this.expirationDate = expirationDate;
+        this.originCountry = originCountry;
+        this.orginization = orginization;
+    }
+    public Organic()
+    {
+
+    }
 
     public String getId() {
         return id;
@@ -35,14 +55,34 @@ public class Organic implements Certification{
     }
 
     public void setDescription(String description) {
-        this.description = description;
+        //Define a regular expression with non special charters and underscore/dash
+        Pattern p = Pattern.compile("[^a-z0-9-_ ]", Pattern.CASE_INSENSITIVE);
+
+        // Creating matcher for expression and our input string
+        Matcher m = p.matcher(description);
+        if(description.length() > 255 )
+        {
+            throw new IllegalArgumentException("description must be less then 255");
+        }
+        else if (m.find())
+        {
+            throw new IllegalArgumentException("String contains special characters");
+        }
+        else
+        {
+            this.description = description;
+        }
     }
 
     public void setCertificationDate(MyDate certificationDate) {
         this.certificationDate = certificationDate;
     }
 
-    public void setExpirationDate(MyDate expirationDate) {
+    public void setExpirationDate(MyDate startdate,MyDate expirationDate) {
+        if(expirationDate.isBefore(startdate))
+        {
+            throw new IllegalArgumentException("ExpirationDate has to be after startdate");
+        }
         this.expirationDate = expirationDate;
     }
 
