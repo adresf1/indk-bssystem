83a5f088f26a805c3882da8ce6dd1d3ca3a1908c
HiroshikunDK <36033304+HiroshikunDK@users.noreply.github.com>
2024-05-13 12:06:02 +0200

    fixed celllist

--- Java/Core/CustomCellFactory/ProductListCell.java
+++ Java/Core/CustomCellFactory/ProductListCell.java
@@ -15,6 +15,7 @@ public class ProductListCell extends ListCell<Product> {
         private Text name;
         private Text price;
         private Text quantity;
+        private Text expirationDate;
 
         private Text unitType;
         private Text totalPrice;
@@ -24,16 +25,18 @@ public class ProductListCell extends ListCell<Product> {
             super();
             name = new Text();
             price = new Text();
-            VBox vBox = new VBox(name, price);
+            expirationDate = new Text();
+            VBox vBox = new VBox(name, price, expirationDate);
             content = new HBox(new Label("[Graphic]"), vBox);
-            content.setSpacing(10);
+            content.setSpacing(20);
         }
     @Override
     protected void updateItem(Product item, boolean empty) {
         super.updateItem(item, empty);
         if (item != null && !empty) { // <== test for null item and empty parameter
             name.setText(item.getName());
-            price.setText(String.format("%d $", item.getPrice()));
+            price.setText(String.format("%1$,.2f", item.getPrice()));
+            expirationDate.setText(item.getExpirationDate().toString());
             setGraphic(content);
         } else {
             setGraphic(null);
--- Java/Core/CustomCellFactory/ProductListView.java
+++ Java/Core/CustomCellFactory/ProductListView.java
@@ -16,16 +16,17 @@ public class ProductListView extends Application {
     //private Product product;
     @Override
     public void start(Stage primaryStage) throws Exception {
-        ObservableList<Product> data = FXCollections.observableArrayList();
+        ObservableList<Product> cart = FXCollections.observableArrayList();
+        ObservableList<Product> displayProducts = FXCollections.observableArrayList();
 
 
-        data.addAll(new Product("Brød", "1", 123, "Wholefood", MyDate.fromString("01/01/2020"), MyDate.fromString("02/08/2020"), 234565, 3.95,7.25,5.55,"stk"),
+        cart.addAll(new Product("Brød", "1", 123, "Wholefood", MyDate.fromString("01/01/2020"), MyDate.fromString("02/08/2020"), 234565, 3.95,7.25,5.55,"stk"),
                     new Product("Mælk", "2", 234, "Grocery", MyDate.fromString("10/01/2020"), MyDate.fromString("23/02/2020"), 345064,7.95, 8.65, 2.25, "liter"),
                     new Product("Æg", "3", 132, "Grocery", MyDate.fromString("11/01/2020"), MyDate.fromString("27/01/2020"), 45063,8.95, 50, 5, "Kg"),
                 new Product("Bacon og Æg", "4", 754, "Breakfast", MyDate.fromString("11/01/2020"), MyDate.fromString("27/01/2020"), 12345,10.95, 40, 2, "Kg")
         );
 
-        final ListView<Product> listView = new ListView<Product>(data);
+        final ListView<Product> listView = new ListView<Product>(cart);
         listView.setCellFactory(new Callback<ListView<Product>, ListCell<Product>>() {
             @Override
             public ListCell<Product> call(ListView<Product> listView) {
